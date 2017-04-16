e8c7cb012acc8b4cf3cde1c61b03ff79d13bdf54
Yos Riady <yosriady@gmail.com>
2017-04-16 20:06:45 +0800

    Layout

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,7 @@
-import Header from '../components/Header'
+import Layout from '../components/MyLayout'
 
 export default () => (
-  <div>
-    <Header />
+  <Layout>
     <p>This is the about page</p>
-  </div>
+  </Layout>
 )
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,16 @@
-import Header from '../components/Header'
+import Layout from '../components/MyLayout'
 import Link from 'next/link'
 
 // Link is a Higher order component that takes a component and returns
 // a new component. https://facebook.github.io/react/docs/higher-order-components.html
 
 const Index = () => (
-  <div>
-    <Header />
+  <Layout>
     <p>This is the index page</p>
     <Link href='/about'>
       <a style={{ fontSize: 20 }}>About</a>
     </Link>
-  </div>
+  </Layout>
 )
 
 export default Index
