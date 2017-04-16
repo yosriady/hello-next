484ffd76885c367b75996a8923e64417838604b8
Yos Riady <yosriady@gmail.com>
2017-04-16 19:16:48 +0800

    Client-side navigation

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
-const Index = () => {
+import Link from 'next/link'
+
+// Link is a Higher order component that takes a component and returns
+// a new component. https://facebook.github.io/react/docs/higher-order-components.html
+
+const Index = () => (
   <div>
-    <p>Hello Next.js</p>
+    <p>This is the index page</p>
+    <Link href='/about'>
+      <a style={{ fontSize: 20 }}>About</a>
+    </Link>
   </div>
-}
+)
 
 export default Index
